639a3c2ee89738709022328498fb9d4657543afa
nogato13 <f.britto@escola.pr.gov.br>
2025-09-16 16:58:29 -0300

    aula do dia 16

--- script.js
+++ script.js
@@ -7,4 +7,35 @@ function mostraPalavrasChave(){
     const palavra = texto.split(" ");
 
     campoResultado.testContent = palavra.join(",");
+
+}
+
+function processaTexto (texto) {
+    let palavra = texto.split(/\P(L)+/u);
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenadaPalavra);
+
+    function ordenadaPalavra(p1,p2){
+ return frequencias [p2]- frequencias[p1];
+        
+    }
+
+    console.log(ordenadas);
+    return ordenadas.slice(0,10);
+
+}
+
+function contaFrequencias(palavras){
+
+let frequencias = ();
+for ( let i of palavras){
+    frequencias [i] = 0;
+    for (let j of palavras){
+        if (i==j){
+            frequencias [i]++;
+        }
+}
+
+    return palavras;
+
 }
